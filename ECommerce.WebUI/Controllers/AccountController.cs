0c18490e3f1980d7620996caaedb4951a42c56e4
Orucl_gr57@itstep.edu.az <Orucl_gr57@itstep.edu.az>
2023-12-22 09:10:57 +0400

    Add project files.

---
+++ ECommerce.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPProject.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View("ForgotPassword");
+        }
+    }
+}
---
+++ ECommerce.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ECommerce.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MyProfile()
+        {
+            return View("MyProfile");
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
