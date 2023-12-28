6e07ac656cc00624febcf13c3a3b3a8aa3dffcba
Orucl_gr57@itstep.edu.az <Orucl_gr57@itstep.edu.az>
2023-12-28 22:13:59 +0400

    updated

--- ECommerce.Entities/CustomIdentityUser.cs
+++ ECommerce.Entities/CustomIdentityUser.cs
@@ -9,6 +9,13 @@ namespace ASPProject.Entities
 {
     public class CustomIdentityUser : IdentityUser<string>
     {
+        public string? Email { get; set; }
+        public DateTime Birthday { get; set; }
+        public string? BirthPlace { get; set; }
+        public string? Occupation { get; set; }
+        public string? Phone { get; set; }
+
+
         public string? ImageUrl { get; set; }
         public bool IsFriend { get; set; }
         public bool IsOnline { get; set; }
